bcb88528f361f8bc176f821d7bade31bd59ff789
rwema3 <rwemadominik3@gmail.com>
2022-02-25 14:29:35 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,3 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:animated_onboarding/screens/onboard/onboard.
+import 'package:animated_onboarding/screens/onboard/onboard.d
