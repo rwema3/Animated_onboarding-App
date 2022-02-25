ac8aae6a5fddf7a96b468ce9fc1b4edb7e4bdd2d
rwema3 <rwemadominik3@gmail.com>
2022-02-25 15:01:28 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,4 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: Onboard(),
-      theme: ThemeData(primaryColor: const Color(0xFF4A00E0)),
     
