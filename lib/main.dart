dee2a4d65d615c2809d905234579c6d30bca5feb
rwema3 <rwemadominik3@gmail.com>
2022-02-25 15:02:09 +0200

    class MyApp extends StatelessWidget {   @override   Widget build(BuildContext context) {     return GetMaterialApp(       home: Onboard(),       theme: ThemeData(primaryColor: const Color(0xFF4A00E0)),     );   } }
    class MyApp extends StatelessWidget {
      @override
      Widget build(BuildContext context) {
        return GetMaterialApp(
          home: Onboard(),
          theme: ThemeData(primaryColor: const Color(0xFF4A00E0)),
        );
      }
    }

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:animated_onboarding/screens/onboard/onboard.dart'
+import 'package:animated_onboarding/screens/onboard/onboard.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,4 +9,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  
+    return GetMaterialApp(
+      home: Onboard(),
+      theme: ThemeData(primaryColor: const Color(0xFF4A00E0)),
+    );
+  }
+}
