5aa88827360b761d5d1e26d8d4cc3013372bc210
rwema3 <rwemadominik3@gmail.com>
2022-02-25 14:53:40 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,4 +11,4 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       home: Onboard(),
-      theme: ThemeData(primaryColor: const
+      theme: ThemeData(primaryColor: const 
