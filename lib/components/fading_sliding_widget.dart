bb43cc8c8d88bce110b280d32ad595b365157477
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-26 23:57:28 +0200

    required cd

--- lib/components/fading_sliding_widget.dart
+++ lib/components/fading_sliding_widget.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 class FadingSlidingWidget extends StatelessWidget {
   const FadingSlidingWidget({
     required AnimationController animationController,
+    required this.child,
