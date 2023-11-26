1656620d5c34e5c36ce16a6755e504cd310ae7e7
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-27 00:59:19 +0200

    components and fadings

--- lib/screens/onboard/onboard.dart
+++ lib/screens/onboard/onboard.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:animated_onboarding/model/onboard_page_item.dart';
+import 'package:animated_onboarding/components/fading_sliding_widget.dart';
+import 'package:animated_onboarding/screens/onboard/welcome_page.dart';
+import 'package:animated_onboarding/screens/onboard/onboard_page.dart';
