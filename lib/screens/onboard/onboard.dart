10026064a05abad4981f25c865791aeeb60787a3
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-27 00:59:35 +0200

    Minscreens

--- lib/screens/onboard/onboard.dart
+++ lib/screens/onboard/onboard.dart
@@ -4,3 +4,5 @@ import 'package:animated_onboarding/model/onboard_page_item.dart';
 import 'package:animated_onboarding/components/fading_sliding_widget.dart';
 import 'package:animated_onboarding/screens/onboard/welcome_page.dart';
 import 'package:animated_onboarding/screens/onboard/onboard_page.dart';
+import 'package:animated_onboarding/screens/mainscreen.dart';
+
